0b87fe97ad2e8f1cb47cd992ab50e60c14c6c06e
Michae1Weiss <misha19989@icloud.com>
2025-01-15 20:50:56 +0100

    Added basic scaffolds to SettingScreen, SettingViewModel, SettingRepository

---
+++ lib/ui/settings/widgets/settings_screen.dart
@@ -0,0 +1,13 @@
+import 'package:cupertino_settings_mvvm_example/ui/settings/view_model/settings_view_model.dart';
+import 'package:flutter/cupertino.dart';
+
+class SettingsScreen extends StatelessWidget{
+  const SettingsScreen({super.key, required this.viewModel});
+
+  final SettingsViewModel viewModel;
+
+  @override
+  Widget build(BuildContext context) {
+    throw UnimplementedError();
+  }
+}
