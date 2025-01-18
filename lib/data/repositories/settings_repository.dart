0743e8187fde13d71cb004270bc117b672210034
Michae1Weiss <misha19989@icloud.com>
2025-01-18 12:24:46 +0100

    Refactored the SettingsRepository, added a boolean setting for a theme

--- lib/data/repositories/settings_repository.dart
+++ lib/data/repositories/settings_repository.dart
@@ -4,19 +4,30 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 class SettingsRepository extends ChangeNotifier {
   SettingsRepository() {
-    readUseMockApiFlag();
+    readSettings();
   }
 
   bool _useMockApiFlag = false;
+  bool _useDarkTheme = false;
 
   bool get useMockApiFlag => _useMockApiFlag;
+  bool get useDarkTheme => _useDarkTheme;
 
   Future<Result<void>> setUseMockApiFlag(bool flag) async {
     // Obtain shared preferences.
     final SharedPreferences prefs = await SharedPreferences.getInstance();
 
-    prefs.setBool("useMockApi", flag);
-    _useMockApiFlag = flag;
+    _useMockApiFlag = await prefs.setBool("useMockApi", flag);
+    notifyListeners();
+
+    return Result.value(null);
+  }
+
+  Future<Result<void>> setUseDarkTheme(bool flag) async {
+    // Obtain shared preferences.
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    _useDarkTheme = await prefs.setBool("useDarkTheme", flag);
     notifyListeners();
 
     return Result.value(null);
@@ -35,12 +46,33 @@ class SettingsRepository extends ChangeNotifier {
     return Result.value(flag);
   }
 
-  Future<void> readUseMockApiFlag() async {
+  Future<Result<bool>> getUseDarkTheme() async {
+    // Obtain shared preferences.
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    bool? flag = prefs.getBool("useDarkTheme");
+
+    if (flag == null) {
+      return Result.error("No flag in the local storage :(");
+    }
+
+    return Result.value(flag);
+  }
+
+  Future<void> readSettings() async {
     var result = await getUseMockApiFlag();
+    var resultUDT = await getUseDarkTheme();
 
     if (result.isValue) {
       _useMockApiFlag = result.asValue!.value;
-      notifyListeners();
     }
+
+    if (resultUDT.isValue) {
+      _useDarkTheme = resultUDT.asValue!.value;
+    }
+
+    if (result.isValue || resultUDT.isValue) {
+      notifyListeners();
+    } 
   }
 }
