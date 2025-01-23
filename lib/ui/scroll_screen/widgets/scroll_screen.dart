506a43ff8ac9458bc41bd2a521fb11e8fac270f6
johny.johny.davis <johny.johny.davis@gmail.com>
2025-01-23 08:55:25 +0000

    Added a bottomAppBar build method to the ScrollScreen

--- lib/ui/scroll_screen/widgets/scroll_screen.dart
+++ lib/ui/scroll_screen/widgets/scroll_screen.dart
@@ -11,7 +11,41 @@ class ScrollScreen extends StatelessWidget {
       ),
       child: CustomScrollView(
         slivers: <Widget>[
-          // Add sliver widgets here
+          SliverToBoxAdapter(
+            child: bottomAppBar(context),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget bottomAppBar(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border(
+          top: BorderSide(
+            width: 0.5,
+            color: CupertinoColors.separator.resolveFrom(context),
+          ),
+        ),
+        color: CupertinoTheme.of(context).barBackgroundColor,
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          CupertinoButton(
+            onPressed: null,
+            minSize: 0,
+            child: Icon(
+              CupertinoIcons.chevron_up,
+              size: 25,
+            ),
+          ),
+          CupertinoButton(
+            onPressed: null,
+            minSize: 0,
+            child: Icon(CupertinoIcons.pause_fill, size: 25),
+          )
         ],
       ),
     );
