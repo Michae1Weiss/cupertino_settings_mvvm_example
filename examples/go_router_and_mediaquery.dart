c6947a4e004dca30106b819abda4ddcc7d15e501
Michae1Weiss <misha19989@icloud.com>
2025-01-30 20:15:28 +0100

    Adjusted the example go_router_and_mediaquery.dart

--- examples/go_router_and_mediaquery.dart
+++ examples/go_router_and_mediaquery.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:go_router/go_router.dart';
+import 'package:sheet/route.dart';
 
 import 'mediequery_sizeof.dart';
 
@@ -8,31 +9,89 @@ GoRouter router = GoRouter(
   routes: [
     GoRoute(
       path: '/',
-      builder: (_, __) => App(),
-    ),
-    GoRoute(
-      path: 'detail',
-      builder: (_, __) => Detail(),
+      pageBuilder: (_, __) => CupertinoExtendedPage(child: App()),
+      routes: [
+        GoRoute(
+          path: 'detail',
+          pageBuilder: (_, __) => CupertinoSheetPage(child: Detail()),
+        ),
+      ],
     ),
   ],
 );
 
 void main() {
-  runApp(CupertinoApp.router(routerConfig: router));
+  runApp(CupertinoApp.router(
+    theme: CupertinoThemeData(brightness: Brightness.light),
+    routerConfig: router,
+  ));
 }
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return CupertinoPageScaffold(
+      navigationBar: CupertinoNavigationBar(
+        middle: Text('Master'),
+      ),
+      child: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              CupertinoButton(
+                onPressed: () => context.go('/detail'),
+                child: Text('Open Detail Sheet'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
 
 class Detail extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return CupertinoPageScaffold(
+      navigationBar: CupertinoNavigationBar(
+        middle: Text('Detail'),
+      ),
+      child: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              Text('Detail'),
+              CupertinoButton(
+                onPressed: () => _openSubSheet(context),
+                child: Text('Show More Details...'),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _openSubSheet(context) {
+    Navigator.of(context).push(
+      CupertinoSheetRoute(builder: (context) => SubDetail()),
+    );
+  }
+}
+
+class SubDetail extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      navigationBar: CupertinoNavigationBar(
+        middle: Text('More Details'),
+      ),
+      child: SafeArea(
+        child: Center(
+          child: Text('More Details'),
+        ),
+      ),
+    );
   }
 }
