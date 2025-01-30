cca1d6e2ff3e9b90b0a2e550f15f4e2689fde0cf
Michae1Weiss <misha19989@icloud.com>
2025-01-30 19:13:15 +0100

    Made the Home app responsive

--- examples/mediequery_sizeof.dart
+++ examples/mediequery_sizeof.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/cupertino.dart';
+import 'package:sheet/route.dart';
+
+import 'cupertino_form_sheet.dart';
 
 void main() {
   return runApp(CupertinoApp(
@@ -16,11 +19,49 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    var size = MediaQuery.sizeOf(context);
+    Size size = MediaQuery.sizeOf(context);
     widget.couter += 1;
     return CupertinoPageScaffold(
-        child: Center(
-      child: Column(),
-    ));
+      child: Center(
+        child: Column(
+          children: [
+            Text('Counter: ${widget.couter}'),
+            CupertinoButton(
+              child: Text('Open a Sheet'),
+              onPressed: () => size.width > 650 ? _openFormSheet(context) : _openSheet(context),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _openSheet(context) {
+    Navigator.of(context).push(
+      CupertinoSheetRoute(
+        builder: (context) => Child(),
+      ),
+    );
+  }
+
+  void _openFormSheet(context) {
+    Navigator.of(context).push(
+      CupertinoFormSheetRoute(
+        builder: (context) => Child(),
+      ),
+    );
+  }
+}
+
+class Child extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      child: Center(
+        child: CupertinoTextField(
+          placeholder: 'Name',
+        ),
+      ),
+    );
   }
 }
