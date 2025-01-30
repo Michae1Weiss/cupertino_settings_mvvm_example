63b5293e1e939a6a6584bc0f568a0e0e48d678c5
Michae1Weiss <misha19989@icloud.com>
2025-01-30 18:42:33 +0100

    Made the Home widget stateful with a counter

--- examples/mediequery_sizeof.dart
+++ examples/mediequery_sizeof.dart
@@ -1,14 +1,20 @@
-
-
 import 'package:flutter/cupertino.dart';
 
 void main() {
   return runApp(CupertinoApp(home: Home(),));
 }
 
-class Home extends StatelessWidget{
+class Home extends StatefulWidget{
+  @override
+  State<Home> createState() => _HomeState();
+
+  int couter = 0;
+}
+
+class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return Text("Hello World!");
+    widget.couter += 1;
+    return Text('Hello World! Counter value: ${widget.couter}');
   }
 }
