3beaa26a56ef235d07e753f088a6a91d51e6ecdf
Michae1Weiss <misha19989@icloud.com>
2025-02-10 18:24:09 +0100

    Update example

--- examples/cupertino_text_form_field_row_suffix.dart
+++ examples/cupertino_text_form_field_row_suffix.dart
@@ -1,32 +1,40 @@
 import 'package:flutter/cupertino.dart';
 
 void main() {
-  runApp(CupertinoApp(home: Home()));
+  runApp(
+    CupertinoApp(
+      home: Home(),
+      theme: CupertinoThemeData(brightness: Brightness.light),
+    ),
+  );
 }
 
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
-      child: Center(
-        child: Row(
-          children: [
-            Expanded(
-              child: CupertinoTextFormFieldRow(
-                prefix: Text('Name'),
-                placeholder: 'Required',
-              ),
-            ),
-            SizedBox(
-              width: 50.0,
-              child: CupertinoListTile(
-                title: Icon(
-                  CupertinoIcons.add,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
+            children: [
+              Expanded(
+                child: CupertinoTextFormFieldRow(
+                  prefix: Text('Name'),
+                  placeholder: 'Required',
                 ),
               ),
-            )
-          ],
-        ),
+              SizedBox(
+                width: 50.0,
+                child: CupertinoListTile(
+                  title: Icon(
+                    CupertinoIcons.add,
+                  ),
+                ),
+              )
+            ],
+          ),
+        ],
       ),
     );
   }
